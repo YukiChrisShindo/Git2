25e63de91c87446e534c8a13fe0d743137f78d3d
Yuki Shindo <yuki.chris.shindo@gmail.com>
2020-12-27 22:23:17 +0900

    Initial Commit

---
+++ Git2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git2
+//
+//  Created by 進藤 優貴 on 2020/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git2/Git2App.swift
@@ -0,0 +1,17 @@
+//
+//  Git2App.swift
+//  Git2
+//
+//  Created by 進藤 優貴 on 2020/12/27.
+//
+
+import SwiftUI
+
+@main
+struct Git2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
